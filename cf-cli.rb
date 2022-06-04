d40686c4a77cb1dc33c2f651ae0afbf7d3ee8373
wanddynosios <67467325+wanddynosios@users.noreply.github.com>
2022-06-04 10:29:35 +0200

    update cloudfoundry cli to v8.4.0; include deprecation warning

--- cf-cli.rb
+++ cf-cli.rb
@@ -4,22 +4,27 @@
 class CfCli < Formula
    desc "The official command line client for Cloud Foundry"
    homepage "https://docs.cloudfoundry.org/cf-cli"
-   url "https://github.com/cloudfoundry/cli/releases/download/v8.3.0/cf-cli_osx_arm"
-   sha256 "212c50401df758e7b0ce55d762e19c8cea51bb1fb5c28e1eeb8f3f114afdfff0"
+   url "https://packages.cloudfoundry.org/homebrew?arch=macosarm&version=8.4.0"
+   sha256 "156261838e3e65d52b5d9eea3f2c53113a6bab05349494cd8f0f7d6e06398e29"
    license "Apache-2.0"
 
-  
+
     # depends_on "cmake" => :build
-  
+
     depends_on :arch => :arm64
-  
+
     def install
-      bin.install 'cf-cli_osx_arm'
-      bin.install_symlink 'cf-cli_osx_arm' => 'cf'
-    end
-  
+      bin.install 'cf8'
+      bin.install_symlink 'cf8' => 'cf'
+      system "echo -n '\n
+\tThis Tap is now deprecated!\n
+\tPlease use the official tap:\n
+\tbrew uninstall wanddynosios/tap/cf-cli\n
+\tbrew install cloudfoundry/tap/cf-cli@8\n'
+"
+     end
+
     test do
       system "#{bin}/cf"
     end
   end
-  
