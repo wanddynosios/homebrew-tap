16a79b647d87d57b2145a9e1f979e43109a03d18
wanddynosios <67467325+wanddynosios@users.noreply.github.com>
2024-02-15 11:55:43 +0100

    add bitami charts-syncer

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 
-# Homebrew Tap for CF CLI
+# Homebrew Tap for darwin/arm64
+`brew tap wanddynosios/tap`
+
+## bitnami charts-syncer
+
+
+## cf cli
+
 A simple homebrew tap for [cf cli](https://github.com/cloudfoundry/cli) v8 for darwin/arm64
 
 ## This tap is now deprecated⚠️ and will not be updated to versions higher than v8.4.0
@@ -8,10 +15,3 @@ Please use the official tap:
 `brew uninstall wanddynosios/tap/cf-cli`
 
 `brew install cloudfoundry/tap/cf-cli@8`
-
-## Install
-`brew install wanddynosios/tap/cf-cli`
-## Upgrade 
-`brew upgrade wanddynosios/tap/cf-cli`
-## Uninstall 
-`brew uninstall wanddynosios/tap/cf-cli`
---
+++ charts-syncer.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class ChartsSyncer < Formula
+    desc "Tool for synchronizing Helm Chart repositories."
+    homepage "https://bitnami.com"
+    url "https://github.com/bitnami/charts-syncer/releases/download/v0.20.3/charts-syncer_0.20.3_darwin_arm64.tar.gz"
+    sha256 "8c6f45ed98eac0eb6242ce02d8cec74c690f953e0c1e3d23a564b740438322d3"
+    license "Apache-2.0"
+  
+    # depends_on "cmake" => :build
+  
+    def install
+      bin.install 'charts-syncer'
+    end
+  
+    test do
+      system "#{bin}/charts-syncer"
+    end
+  end
+  
